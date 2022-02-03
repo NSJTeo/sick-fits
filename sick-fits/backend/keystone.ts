9d91db0d061880fcac90e6d4072acea5650fdc6b
nsjteo <n.teomacdonald@gmail.com>
2022-02-03 00:31:56 -0800

    Lesson 12 - Initialize Keystone

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+export default config({
+  server: {
+    cors: {
+      origin: [process.env.FRONTEND_URL],
+      credentials: true,
+    },
+  },
+  db: {
+    adapter: 'mongoose',
+    url: databaseURL,
+    // TODO: add data seeding here
+  },
+  lists: createSchema({
+    // Schema items go here
+  }),
+  ui: {
+    // TODO: change this for roles
+    isAccessAllowed: () => true,
+  },
+  // TODO: add session values
+});
