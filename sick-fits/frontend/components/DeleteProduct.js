12cb135aae1f11ab40a235ddcf7e9f75a5d6cdc2
nsjteo <n.teomacdonald@gmail.com>
2022-03-02 03:00:08 -0800

    Lesson 30 - Delete product functionality

---
+++ sick-fits/frontend/components/DeleteProduct.js
@@ -0,0 +1,30 @@
+import { useMutation } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const DELETE_PRODUCT_MUTATION = gql`
+  mutation DELETE_PRODUCT_MUTATION($id: ID!) {
+    deleteProduct(id: $id) {
+      id
+      name
+    }
+  }
+`;
+
+export default function DeleteProduct({ id, children }) {
+  const [deleteProduct, { loading }] = useMutation(DELETE_PRODUCT_MUTATION, {
+    variables: { id },
+  });
+
+  const handleClick = () => {
+    if (confirm('Are you sure you want to delete this item?')) {
+      deleteProduct().catch((error) => {
+        alert(error);
+      });
+    }
+  };
+  return (
+    <button type="button" onClick={handleClick} disabled={loading}>
+      {children}
+    </button>
+  );
+}
