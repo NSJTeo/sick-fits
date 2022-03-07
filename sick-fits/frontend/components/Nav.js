1c2b82adb3b1e448bba769fe2b7226658df75100
nsjteo <n.teomacdonald@gmail.com>
2022-03-06 16:36:36 -0800

    Lesson 48 - Add to cart animation

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -4,6 +4,7 @@ import NavStyles from './styles/NavStyles';
 import useUser from './User';
 import SignOut from './SignOut';
 import { useCart } from '../lib/cartState';
+import CartCount from './CartCount';
 
 export default function Nav() {
   const user = useUser();
@@ -19,15 +20,16 @@ export default function Nav() {
           <SignOut />
           <button type="button" onClick={openCart}>
             My Cart
+            <CartCount
+              count={user.cart.reduce(
+                (tally, cartItem) => tally + cartItem.quantity,
+                0
+              )}
+            />
           </button>
         </>
       )}
-      {!user && (
-        <>
-          <Link href="/signin">Sign In</Link>
-        </>
-      )}
-      {/* <Link href="/Cart">Cart</Link> */}
+      {!user && <Link href="/signin">Sign In</Link>}
     </NavStyles>
   );
 }
