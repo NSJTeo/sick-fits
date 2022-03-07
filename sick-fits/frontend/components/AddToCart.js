e34f9ae2236f2144c8d43c43bf5f398cd6e3fb6a
nsjteo <n.teomacdonald@gmail.com>
2022-03-06 17:33:22 -0800

    Adding item to cart opens cart

--- sick-fits/frontend/components/AddToCart.js
+++ sick-fits/frontend/components/AddToCart.js
@@ -1,6 +1,7 @@
 import { useMutation } from '@apollo/client';
 import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
+import { useCart } from '../lib/cartState';
 
 const ADD_TO_CART_MUTATION = gql`
   mutation ADD_TO_CART_MUTATION($id: ID!) {
@@ -11,12 +12,17 @@ const ADD_TO_CART_MUTATION = gql`
 `;
 
 export default function AddToCart({ id }) {
+  const { openCart } = useCart();
   const [addToCart, { loading }] = useMutation(ADD_TO_CART_MUTATION, {
     variables: { id },
     refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
+  const handleClick = () => {
+    addToCart();
+    openCart();
+  };
   return (
-    <button type="button" disabled={loading} onClick={addToCart}>
+    <button type="button" disabled={loading} onClick={handleClick}>
       Add{loading && 'ing'} to Cart
     </button>
   );
